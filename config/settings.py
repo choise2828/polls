ed03d0bef6e0196e37a2e47435b0b4e687c0dc02
choise28 <choise28@gmail.com>
2021-10-16 21:13:47 +0900

    add

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '!6bdhz^^3$x!t&2zz&cqc72rqz9r^j!g-)x&hkj^*(sr!s7ckq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
